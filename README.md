1d008804966c7aa4af5d9a4bb9e450cd30db5d16
PlutoHD <dev@plutohd.de>
2021-03-28 13:51:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_screen_streaming
+ 
---
+++ example/android/app/src/main/kotlin/de/plutohd/flutter_screen_streaming_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.plutohd.flutter_screen_streaming_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
